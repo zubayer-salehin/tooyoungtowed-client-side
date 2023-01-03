e5a357cf4c985f11df4ee54a261d7ce57c827ae3
ishrafil2233 <ishrafil2233@gmail.com>
2023-01-03 17:49:12 +0600

    add US Programming Component

--- src/pages/Main/UsProgramming/UsProgramming.js
+++ src/pages/Main/UsProgramming/UsProgramming.js
@@ -1,10 +1,84 @@
 import React from 'react';
+import './UsProgramming.css'
 
 const UsProgramming = () => {
     return (
-        <div>
-            Us Programming Page
+
+        <div className="bg-[#efe4ce]">
+            {/* start banner  */}
+            <div>
+                <div className='banner'>
+                    <img className='bannerImage' src="https://i.ibb.co/xFhqnPX/banner.jpg" alt="" />
+                    <h1 className='lg:text-5xl text-4xl font-bold text-center text-white'>US PROGRAMMING</h1>
+                </div>
+
+                <img className='w-full' src="https://tooyoungtowed.org/assets/redpattern-border_distressed_bottom-b2bf99437a80fd84fcd6eedc5ea5b9b8e0d42e77a4cfe9de4504e94e8d6e8374.png" alt="" />
+            </div>
+            {/* end banner  */}
+
+            {/* start Girls Article  */}
+            <article className='mt-[53px] mb-[40px] lg:px-[104.5px] px-2'>
+                <div className='container mx-auto '>
+                    <h1 className='text-5xl font-bold text-center mb-[30px] text-[#8c0804]'>US GIRLS' CHOICE INITIATIVE</h1>
+                    <p className='text-[22px] font-normal mb-[30px]'>At TYTW, our core belief is that every girl should be free to determine the course of her own life. We envision a world where adolescent girls are free to be children and teens with access to all levels of education and healthcare. Every day TYTW is committed to the fight for bodily autonomy, which includes reproductive rights, access to sexual and reproductive health resources, and access to safe abortion services.</p>
+                    <p className='text-[22px] font-normal mb-[30px]'>In this time of alarming rollbacks of rights for women and girls, TYTW is launching its Girls Choice Initiative, which provides support for girls under the age of 18 in need of abortion services in the US. Through the program, TYTW engages in advocacy and grantmaking to organizations providing financial, logistical, and psychosocial support to girls seeking abortion care.</p>
+                    <p className='text-[22px] font-normal mb-[30px]'>Read more about our US Girls' Choice Initiative <a href="/">here</a> .</p>
+                </div>
+
+            </article>
+            <img className='w-full' src="https://tooyoungtowed.org/assets/redpattern-border_distressed_bottom-b2bf99437a80fd84fcd6eedc5ea5b9b8e0d42e77a4cfe9de4504e94e8d6e8374.png" alt="" />
+            {/* end Girls Article  */}
+
+            {/* parallax start  */}
+            <article className='parallax'>
+                <div className='parallax-container'>
+
+                </div>
+            </article>
+            {/* parallax end  */}
+            <img className='w-full' src="https://tooyoungtowed.org/assets/redpattern-border_distressed_bottom-b2bf99437a80fd84fcd6eedc5ea5b9b8e0d42e77a4cfe9de4504e94e8d6e8374.png" alt="" />
+
+            {/* Start RESILIENT GIRLS  */}
+            <article>
+                <div>
+                    <div className='flex justify-center my-[40px]'>
+                        <img src="https://tooyoungtowed.org/assets/lotus-gradient-5f6dd739c2805bfaaad2b11d1a5dc9f8cd1b120ae0bc16aba019f9ba6db2f363.png" alt="" />
+                    </div>
+                    <div className='container mx-auto lg:px-[104.5px] px-2'>
+                        <h1 className='text-4xl font-bold text-center mb-[30px] text-[#8c0804]'>RESILIENT GIRLS</h1>
+                        <p className='text-[22px] font-normal mb-[30px]'>TYTW's virtual U.S.-based program launched in Fall 2020 in acknowledgement of the myriad complexities - and opportunities - faced by girls in these times. Resilient Girls is meant to help inspire girls globally to imagine, inspire and build a better world, together, and to support each other as we strive to make these dreams our reality. This virtual advocacy and empowerment program facilitates conversation between female youth leaders worldwide, spotlighting crucial issues as they exchange stories of strength, survival, and perseverance. Each Resilient Girls story features an interview facilitated by an American girl, with a diverse roster of interview subjects ranging from survivors of trauma, youth leaders, and pioneering professionals, highlighting stories of hope and resilience. TYTW thanks program partner <a href="/p">NewEra Creative</a>  <a href="/p">Space</a>, sponsor <a href="/p">Canon USA</a> and strategic advisement partner <a href="/p">Fenton</a> for their integral roles in producing Resilient Girls.
+
+                        </p>
+                        <p className='text-[22px] font-normal '>View the first episode of Resilient Girls <a href="/p">here</a>, a conversation-style interview between Kenyan child marriage survivor Rosillah and Sienna, a second year Harvard student and TYTW's inaugural Youth Ambassador.</p>
+                    </div>
+                </div>
+            </article>
+            {/* End RESILIENT GIRLS  */}
+
+            {/* start reporting article  */}
+            <article>
+                <div className='container mx-auto lg:px-[104.5px] px-2'>
+                    <h1 className='text-4xl font-bold text-center my-[40px] mt-20 text-[#8c0804]'>ORIGINAL REPORTING</h1>
+                    <p className='text-[22px] font-normal '>TYTW’s original reporting includes on-the-ground investigations into the practice of child marriage in the U.S. Through investigative research and interviews with U.S.-based child marriage survivors, those highly vulnerable to the practice, and community leaders who orchestrate the practice, TYTW contributes journalistically to the body of evidence documenting the practice of child, early, and forced marriages within the U.S. where more than 200,000 minors—some as young as 12 years old—were legally married between 2000 and 2015 (with the vast majority being between a girl and an adult male).</p>
+                    <img className='w-full my-[40px] lg:px-8' src="https://i.ibb.co/z5npbNn/programming.jpg" alt="" />
+                </div>
+            </article>
+            {/* end reporting article  */}
+
+            {/* Start Programming article  */}
+            <article>
+                <div className='container mx-auto lg:px-[104.5px] px-2'>
+                    <h1 className='text-4xl font-bold text-center my-[40px] mt-20 text-[#8c0804]'>Youth Ambassador and Advocacy Programming </h1>
+                    <p className='text-[22px] font-normal '>TYTW offers Youth Ambassador and Advocacy programming, in order to equip the next generation of informed, empowered domestic advocates dedicated to ending child marriage and supporting survivors. Through a purposeful combination of in-person organizing and digital engagement through various social media channels and partners, the Youth Ambassador and Advocacy program acts as a vehicle to incorporate young voices into TYTW's girl-centered programming. While developing cross-cultural communication skills and expanding their global knowledge, student participants help raise awareness on the issue of child marriage and of successful campaigns by girls, their families and communities who have halted the practice.</p>
+
+                    <div className='flex justify-center py-[40px]'>
+                        <img src="https://tooyoungtowed.org/assets/lotus-gradient-5f6dd739c2805bfaaad2b11d1a5dc9f8cd1b120ae0bc16aba019f9ba6db2f363.png" alt="" />
+                    </div>
+                </div>
+            </article>
+            {/* End Programming article  */}
         </div>
+
     );
 };
 
