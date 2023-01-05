53097b69332a6d97260d13c330a26d0b7384b850
zubayer-salehin <freelancerzubear@gmail.com>
2023-01-05 14:51:24 +0600

    banner part done

--- src/pages/Main/Home/Banner/Banner.js
+++ src/pages/Main/Home/Banner/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
     return (
         <div className='relative'>
             <img className='bannerImage' src={bannerImage} alt="" />
-            <h3 className='absolute top-[36%] left-[16%] text-5xl font-bold text-white'>LATEST NEWS</h3>
+            <h3 className='absolute top-[40%] left-[14%] text-5xl font-bold text-white'>TooYoung Web</h3>
             <img className='w-[100%] h-[33.7px]' src={flowerImage} alt="" />
         </div>
     );
