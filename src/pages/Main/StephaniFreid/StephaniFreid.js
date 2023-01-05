725bbde957f8db820cfa2224f82079ad806d1581
ishrafil2233 <ishrafil2233@gmail.com>
2023-01-05 18:53:08 +0600

    add stephanie component

--- src/pages/Main/StephaniFreid/StephaniFreid.js
+++ src/pages/Main/StephaniFreid/StephaniFreid.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import './StephaniFreid.css'
 
 const StephaniFreid = () => {
     return (
-        <div>
-            Stephani Freid Page
+        <div className="bg-[#efe4ce]">
+            {/* start banner  */}
+            <div>
+                <div className='banners'>
+                    <img className='bannerImage' id='ban' src="https://i.ibb.co/vQCSbgK/stephanie-header.jpg" alt="" />
+                    <h1 className='lg:text-4xl text-4xl font-bold lg:text-justify text-start mt-[70px] text-white '>STEPHANIE FREID-PERENCHIO <br /> SCHOLARSHIP FUND</h1>
+                </div>
+
+                <img className='w-full' src="https://tooyoungtowed.org/assets/redpattern-border_distressed_bottom-b2bf99437a80fd84fcd6eedc5ea5b9b8e0d42e77a4cfe9de4504e94e8d6e8374.png" alt="" />
+            </div>
+            {/* end banner  */}
+
+            {/* start Girls Article  */}
+            <article className='lg:mt-[53px] mt-[40px] mb-[40px] lg:px-[104.5px] px-5 lg:text-justify'>
+                <div className='container mx-auto '>
+                    <h1 className='lg:text-2xl font-bold mb-[30px]'>Too Young to Wed (TYTW) is delighted to announce initial plans for the launch of the Stephanie Freid-Perenchio Scholarship Fund.</h1>
+                    <p className='text-[22px] font-normal mb-[30px]'>It has been a privilege for TYTW to partner over the years with Perenchio, who has become a pillar of the TYTW community. Perenchio's service as Advisory Board member with TYTW, her generous philanthropic investment in empowering girls and ending child marriage globally, and her own remarkable career as a photographer and human rights advocate, are deeply inspiring. In tribute to Perenchio's transformative impact on the girls TYTW serves, we are pleased to honor her by establishing a scholarship fund in her name.</p>
+                    <p className='text-[22px] font-normal mb-[30px]'>The Stephanie Freid-Perenchio Scholarship Fund helps TYTW meet rising demand for our scholarships, especially in meeting the objective of providing long-term schooling assistance for some of the most profoundly traumatized survivors served by TYTW. Education access is the single-most effective, evidence-based intervention in preventing child marriage. Globally, the increase in education disruptions due to COVID-19 has been most devastating for underprivileged girls, with rising pressure on them to contribute to household incomes. It also halted access to social services and other protective institutional measures. Tens of millions of girls may never return to school due to the pandemic, and are consequently more vulnerable to child marriage, early pregnancy, and domestic violence. The expansion of TYTW’s scholarship assistance programming through the Stephanie Freid-Perenchio Scholarship Fund helps to mitigate the gendered impacts of the pandemic in this urgent time.</p>
+                    <p className='text-[22px] font-normal mb-[30px]'>On behalf of the outstanding girls TYTW is privileged to serve, we celebrate the contributions of Perenchio in making so many empowering resources possible, and are thrilled to launch her eponymous scholarship fund that will create pathways for more girls to thrive.</p>
+                </div>
+
+            </article>
+            <img className='w-full' src="https://tooyoungtowed.org/assets/redpattern-border_distressed_bottom-b2bf99437a80fd84fcd6eedc5ea5b9b8e0d42e77a4cfe9de4504e94e8d6e8374.png" alt="" />
+            {/* end Girls Article  */}
+
+            {/* parallax start  */}
+            <article className='parallaxs'>
+                <div className='parallaxs-container'>
+
+                </div>
+            </article>
+            {/* parallax end  */}
+            <img className='w-full' src="https://tooyoungtowed.org/assets/redpattern-border_distressed_bottom-b2bf99437a80fd84fcd6eedc5ea5b9b8e0d42e77a4cfe9de4504e94e8d6e8374.png" alt="" />
+
+            {/* Start RESILIENT GIRLS  */}
+            <article className='px-5 lg:text-justify'>
+                <div>
+                    <div className='flex justify-center my-[40px]'>
+                        <img src="https://tooyoungtowed.org/assets/lotus-gradient-5f6dd739c2805bfaaad2b11d1a5dc9f8cd1b120ae0bc16aba019f9ba6db2f363.png" alt="" />
+                    </div>
+                    <div className='container mx-auto lg:px-[104.5px] px-2'>
+                        <p className='text-[22px] font-normal mb-[30px]'> <span className='font-bold'>Keeping girls in school, and out of child marriage, helps protect girls from violence.</span> When a girl is forced into marriage, she is robbed of her bodily autonomy, her opportunity to complete her education, and her physical and emotional wellbeing. She is subjected to early, forced, and frequent sexual relations, regardless of her own physical limitations, and more prone to serious health complications and injuries. When a girl is in school, she benefits from additional institutional protective measures, access to resources, and supervision of caring adults.</p>
+
+                        <p className='text-[22px] font-normal mb-[30px]'> <span className='font-bold'> Keeping girls in school, and out of child marriage, reduces infant and maternal mortality rates.</span> Child mothers under age 15 are five times more likely to die in childbirth than women in their twenties. Infants born to child mothers are at higher risk for health complications.</p>
+
+                        <p className='text-[22px] font-normal'><span className='font-bold'>Keeping girls in school, and out of child marriage, helps position families for greater economic security.</span> Girls who stay in school are better equipped to help support their families in later years.</p>
+                        <p className='text-[22px] font-normal mb-[30px]'><span className='font-bold'>Keeping girls in school, and out of child marriage, helps support the planet.</span> Educated girls are more likely to make more environmentally-conscious choices, helping to reduce the effects of climate change.</p>
+
+                        <p className='text-[22px] font-normal mb-[30px]'>You're also driving generational change -- as each girl who evades child marriage is more likely to prevent her own children from marrying early.</p>
+                        <span className='font-bold'></span>
+                        <p className='text-[22px] font-bold mb-[30px]'>To donate, visit TYTW's <a href="d">Give Today page</a> and under the “Designation” drop-down menu, select “Stephanie Freid-Perenchio Scholarship Fund.”</p>
+                        <div className='flex justify-center py-[40px]'>
+                            <img src="https://tooyoungtowed.org/assets/lotus-gradient-5f6dd739c2805bfaaad2b11d1a5dc9f8cd1b120ae0bc16aba019f9ba6db2f363.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </article>
+            {/* End RESILIENT GIRLS  */}
         </div>
     );
 };
--- src/pages/Main/UsProgramming/UsProgramming.js
+++ src/pages/Main/UsProgramming/UsProgramming.js
@@ -3,7 +3,6 @@ import './UsProgramming.css'
 
 const UsProgramming = () => {
     return (
-
         <div className="bg-[#efe4ce]">
             {/* start banner  */}
             <div>
@@ -17,9 +16,9 @@ const UsProgramming = () => {
             {/* end banner  */}
 
             {/* start Girls Article  */}
-            <article className='mt-[53px] mb-[40px] lg:px-[104.5px] px-2'>
+            <article className='lg:mt-[53px] mt-[20px] mb-[40px] lg:px-[104.5px] px-5 lg:text-justify'>
                 <div className='container mx-auto '>
-                    <h1 className='text-5xl font-bold text-center mb-[30px] text-[#8c0804]'>US GIRLS' CHOICE INITIATIVE</h1>
+                    <h1 className='lg:text-5xl text-2xl font-bold lg:text-center lg:mb-[30px] mb-[20px] text-[#8c0804]'>US GIRLS' CHOICE INITIATIVE</h1>
                     <p className='text-[22px] font-normal mb-[30px]'>At TYTW, our core belief is that every girl should be free to determine the course of her own life. We envision a world where adolescent girls are free to be children and teens with access to all levels of education and healthcare. Every day TYTW is committed to the fight for bodily autonomy, which includes reproductive rights, access to sexual and reproductive health resources, and access to safe abortion services.</p>
                     <p className='text-[22px] font-normal mb-[30px]'>In this time of alarming rollbacks of rights for women and girls, TYTW is launching its Girls Choice Initiative, which provides support for girls under the age of 18 in need of abortion services in the US. Through the program, TYTW engages in advocacy and grantmaking to organizations providing financial, logistical, and psychosocial support to girls seeking abortion care.</p>
                     <p className='text-[22px] font-normal mb-[30px]'>Read more about our US Girls' Choice Initiative <a href="/">here</a> .</p>
@@ -39,13 +38,13 @@ const UsProgramming = () => {
             <img className='w-full' src="https://tooyoungtowed.org/assets/redpattern-border_distressed_bottom-b2bf99437a80fd84fcd6eedc5ea5b9b8e0d42e77a4cfe9de4504e94e8d6e8374.png" alt="" />
 
             {/* Start RESILIENT GIRLS  */}
-            <article>
+            <article className='px-5 lg:text-justify'>
                 <div>
                     <div className='flex justify-center my-[40px]'>
                         <img src="https://tooyoungtowed.org/assets/lotus-gradient-5f6dd739c2805bfaaad2b11d1a5dc9f8cd1b120ae0bc16aba019f9ba6db2f363.png" alt="" />
                     </div>
                     <div className='container mx-auto lg:px-[104.5px] px-2'>
-                        <h1 className='text-4xl font-bold text-center mb-[30px] text-[#8c0804]'>RESILIENT GIRLS</h1>
+                        <h1 className='lg:text-4xl text-2xl font-bold lg:text-center mb-[30px] text-[#8c0804]'>RESILIENT GIRLS</h1>
                         <p className='text-[22px] font-normal mb-[30px]'>TYTW's virtual U.S.-based program launched in Fall 2020 in acknowledgement of the myriad complexities - and opportunities - faced by girls in these times. Resilient Girls is meant to help inspire girls globally to imagine, inspire and build a better world, together, and to support each other as we strive to make these dreams our reality. This virtual advocacy and empowerment program facilitates conversation between female youth leaders worldwide, spotlighting crucial issues as they exchange stories of strength, survival, and perseverance. Each Resilient Girls story features an interview facilitated by an American girl, with a diverse roster of interview subjects ranging from survivors of trauma, youth leaders, and pioneering professionals, highlighting stories of hope and resilience. TYTW thanks program partner <a href="/p">NewEra Creative</a>  <a href="/p">Space</a>, sponsor <a href="/p">Canon USA</a> and strategic advisement partner <a href="/p">Fenton</a> for their integral roles in producing Resilient Girls.
 
                         </p>
@@ -56,19 +55,19 @@ const UsProgramming = () => {
             {/* End RESILIENT GIRLS  */}
 
             {/* start reporting article  */}
-            <article>
-                <div className='container mx-auto lg:px-[104.5px] px-2'>
-                    <h1 className='text-4xl font-bold text-center my-[40px] mt-20 text-[#8c0804]'>ORIGINAL REPORTING</h1>
-                    <p className='text-[22px] font-normal '>TYTW’s original reporting includes on-the-ground investigations into the practice of child marriage in the U.S. Through investigative research and interviews with U.S.-based child marriage survivors, those highly vulnerable to the practice, and community leaders who orchestrate the practice, TYTW contributes journalistically to the body of evidence documenting the practice of child, early, and forced marriages within the U.S. where more than 200,000 minors—some as young as 12 years old—were legally married between 2000 and 2015 (with the vast majority being between a girl and an adult male).</p>
+            <article >
+                <div className='container mx-auto lg:px-[104.5px] px-2 lg:text-justify'>
+                    <h1 className='lg:text-4xl text-2xl font-bold lg:text-center my-[40px] mt-20 text-[#8c0804] px-5'>ORIGINAL REPORTING</h1>
+                    <p className='text-[22px] font-normal px-5'>TYTW’s original reporting includes on-the-ground investigations into the practice of child marriage in the U.S. Through investigative research and interviews with U.S.-based child marriage survivors, those highly vulnerable to the practice, and community leaders who orchestrate the practice, TYTW contributes journalistically to the body of evidence documenting the practice of child, early, and forced marriages within the U.S. where more than 200,000 minors—some as young as 12 years old—were legally married between 2000 and 2015 (with the vast majority being between a girl and an adult male).</p>
                     <img className='w-full my-[40px] lg:px-8' src="https://i.ibb.co/z5npbNn/programming.jpg" alt="" />
                 </div>
             </article>
             {/* end reporting article  */}
 
             {/* Start Programming article  */}
-            <article>
+            <article className='px-5 lg:text-justify'>
                 <div className='container mx-auto lg:px-[104.5px] px-2'>
-                    <h1 className='text-4xl font-bold text-center my-[40px] mt-20 text-[#8c0804]'>Youth Ambassador and Advocacy Programming </h1>
+                    <h1 className='lg:text-4xl text-2xl font-bold lg:text-center my-[40px] mt-20 text-[#8c0804]'>Youth Ambassador and Advocacy Programming </h1>
                     <p className='text-[22px] font-normal '>TYTW offers Youth Ambassador and Advocacy programming, in order to equip the next generation of informed, empowered domestic advocates dedicated to ending child marriage and supporting survivors. Through a purposeful combination of in-person organizing and digital engagement through various social media channels and partners, the Youth Ambassador and Advocacy program acts as a vehicle to incorporate young voices into TYTW's girl-centered programming. While developing cross-cultural communication skills and expanding their global knowledge, student participants help raise awareness on the issue of child marriage and of successful campaigns by girls, their families and communities who have halted the practice.</p>
 
                     <div className='flex justify-center py-[40px]'>
